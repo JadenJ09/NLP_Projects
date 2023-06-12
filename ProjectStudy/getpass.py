b53c104a1d0c57d5a7a43e5dd183a6fc031745c4
Jaden_Tal <36606170+JadenJ09@users.noreply.github.com>
2023-06-12 16:48:09 +0200

    Delete ProjectStudy directory

--- ProjectStudy/getpass.py
+++
@@ -1,3 +0,0 @@
-openai_api = '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'
-google_api = 'AIzaSyCIGuLmSrM65sXknalTE4B4x8PsCZpAZ-I'
-google_cx =  'd4c72f123415549d9'
--- ProjectStudy/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-from langchain.document_loaders import YoutubeLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.llms import OpenAI
-from langchain import PromptTemplate
-from langchain.chains import LLMChain
-from dotenv import find_dotenv, load_dotenv
-import textwrap
